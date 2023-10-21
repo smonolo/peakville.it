9135557bfc02c70437e06ec490503f9ba7ac374b
smonolo <lu3mtex@gmail.com>
2023-10-21 23:17:51 +0200

    feat(error): add go back button

--- error.vue
+++ error.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex h-screen w-screen flex-col items-center justify-center">
-    <div class="text-3xl font-bold">Dove sei finito?</div>
-    <div class="mt-2 text-lg">
-      La pagina che stai cercando sembrerebbe non esistere.
-    </div>
+    <div class="font-heading text-3xl font-bold">Dove sei finito?</div>
+    <div class="mt-2 text-lg">La pagina che stai cercando non esiste.</div>
+    <NuxtLink
+      to="/"
+      class="font-heading mt-8 w-fit rounded-xl border-2 border-p-beige-100 bg-p-blue-500 px-10 py-3 text-p-beige-500 outline outline-2 outline-p-blue-900 transition-colors hover:bg-p-blue-900"
+    >
+      Torna Indietro
+    </NuxtLink>
   </div>
 </template>
