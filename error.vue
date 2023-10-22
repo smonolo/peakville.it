ca790a136d09d2ab84067b25e1e2d811fc4d33ba
smonolo <lu3mtex@gmail.com>
2023-10-22 14:24:07 +0200

    feat(error): add title to error page

--- error.vue
+++ error.vue
@@ -4,9 +4,13 @@
     <div class="mt-2 text-lg">La pagina che stai cercando non esiste.</div>
     <NuxtLink
       to="/"
-      class="font-heading mt-8 w-fit rounded-xl border-2 border-p-beige-100 bg-p-blue-500 px-8 py-3 text-p-beige-500 outline outline-2 outline-p-blue-900 transition-colors hover:bg-p-blue-900"
+      class="mt-8 w-fit rounded-xl border-2 border-p-beige-100 bg-p-blue-500 px-8 py-3 font-heading text-p-beige-500 outline outline-2 outline-p-blue-900 transition-colors hover:bg-p-blue-900"
     >
       Torna Indietro
     </NuxtLink>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({ title: "404 - Peakville" })
+</script>
